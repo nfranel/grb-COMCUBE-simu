9f345d3dddf2f109ddf1830ac3195580eafa7e4c
nfranel <nathan.franel@ijclab.in2p3.fr>
2023-06-27 10:49:14 +0200

    New graph added

--- dataclass.py
+++ dataclass.py
@@ -1424,7 +1424,7 @@ class AllSourceData:
     ax1.set(xlabel="Polarization angle (°)", ylabel="Number of detection per year", yscale=y_scale)
     plt.show()
 
-  def mdp_vs_fluence(self, selected_sat="const", mdp_threshold=1, y_scale="log"):
+  def mdp_vs_fluence(self, selected_sat="const", mdp_threshold=1):
     """
 
     """
@@ -1467,7 +1467,7 @@ class AllSourceData:
       ax1.legend()
       plt.show()
 
-  def mdp_vs_pflux(self, selected_sat="const", mdp_threshold=1, y_scale="log"):
+  def mdp_vs_pflux(self, selected_sat="const", mdp_threshold=1):
     """
 
     """
@@ -1509,7 +1509,7 @@ class AllSourceData:
     ax1.legend()
     plt.show()
 
-  def mdp_vs_detection_angle(self, selected_sat=0, mdp_threshold=1, y_scale="log"):
+  def mdp_vs_detection_angle(self, selected_sat=0, mdp_threshold=1):
     """
 
     """
@@ -1543,7 +1543,7 @@ class AllSourceData:
     ax1.legend()
     plt.show()
 
-  def snr_vs_fluence(self, selected_sat="const", snr_threshold=5, y_scale="log"):
+  def snr_vs_fluence(self, selected_sat="const", snr_threshold=5):
     """
 
     """
@@ -1586,7 +1586,7 @@ class AllSourceData:
       ax1.legend()
       plt.show()
 
-  def snr_vs_pflux(self, selected_sat="const", snr_threshold=5, y_scale="log"):
+  def snr_vs_pflux(self, selected_sat="const", snr_threshold=5):
     """
 
     """
@@ -1628,7 +1628,7 @@ class AllSourceData:
     ax1.legend()
     plt.show()
 
-  def snr_vs_detection_angle(self, selected_sat=0, snr_threshold=5, y_scale="log"):
+  def snr_vs_detection_angle(self, selected_sat=0, snr_threshold=5):
     """
 
     """
@@ -1662,65 +1662,43 @@ class AllSourceData:
     ax1.legend()
     plt.show()
 
-# init_time = time()
-# bkg = "./backgrounds/bkg"  # _background_sat0_0000_90.0_0.0.inc1.id1.extracted.tra"
-# param = "./wobkg/polGBM.par"
-# erg = (100, 460)
-# arm = 180
-# test = AllSourceData(bkg, param, erg, arm, parallel=True)
-# test.make_const()
-# test.analyze()
-# print("=======================================")
-# print("processing time : ", time()-init_time, "seconds")
-# print("=======================================")
-#
-# set_bin = np.linspace(-180, 180, 21)
-# set_trafile = test.alldata[0][3][0]
-# test_values = set_trafile.arm
-# set_trafile.pol.show(set_trafile.unpol)
-#
-# fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
-# binw = set_bin[1:]-set_bin[:-1]
-# binpos = 0.5 * (set_bin[1:] + set_bin[:-1])
-# ax1.hist(set_trafile.pol, bins=set_bin)
-# ax2.hist(set_trafile.unpol, bins=set_bin)
-# pol = np.histogram(set_trafile.pol, set_bin)[0] / binw
-# unpol = np.histogram(set_trafile.unpol, set_bin)[0] / binw
-# ax3.scatter(binpos, pol / unpol * np.mean(unpol))
-# xvar = np.linspace(-180, 180, 1000)
-# yvar = 0.57911 - 0.231988*np.cos((2*(xvar-17.0009))*np.pi/180)
-# ax3.plot(xvar, yvar, color="orange")
-# ax3.scatter(binpos, modulation_func(binpos, *set_trafile.pol.fits[-2].popt))
-# ax3.errorbar(binpos, pol / unpol * np.mean(unpol), yerr=set_trafile.pol.polarigram_error, fmt = 'none')
-# print(f"Modulation:           {set_trafile.mu100}+-{set_trafile.mu100_err}")
-# print(f"Polarization angle:   {set_trafile.pa}+-{set_trafile.pa_err}")
-# plt.show()
-# print("cov : ", set_trafile.pol.fits[-2].pcov)
-# print("PA, mu, S : ", set_trafile.pol.fits[-2].popt)
-# print(set_trafile.pol.polarigram_error)
+  def mdp_vs_snr(self, selected_sat="const", snr_threshold=5, mdp_threshold=1, print_rejected=False):
+    """
 
-# from time import sleep
-# from itertools import repeat
-# def functest(intro, timeval, message1, message2):
-#  sleep(timeval)
-#  liste.append(timeval)
-# return [timeval, message]
-#  return f"{intro} {timeval}{message1}{message2}"
-# intro = "il faut attendre un peu, environ"
-# message1 = "secondes. "
-# message2 = "On peut fermer le programme maintenant"
-# message = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-# timeval = np.random.random(10)
-# functest(timeval[0], message[0])
-# init_time = time()
-# valtest = list(map(functest, intro, timeval, message1, message2))
-# print(valtest)
-# print(time()-init_time)
-# init_time = time()
-# with mp.Pool() as pool:
-#  val = pool.starmap(functest, zip(repeat(intro), timeval, repeat(message1), repeat(message2)))
+    """
+    mdp_list = []
+    snr_list = []
+    count = 0
+    no_detec = [[],[]]
+    for source_ite, source in enumerate(self.alldata):
+      if source is not None:
+        for sim in source:
+          if sim is not None:
+            if selected_sat == "const":
+              if sim.const_data.snr >= snr_threshold and sim.const_data.mdp <= mdp_threshold:
+                mdp_list.append(sim.const_data.mdp * 100)
+                snr_list.append(sim.const_data.snr)
+              else:
+                no_detec[0].append(sim.const_data.mdp * 100)
+                no_detec[1].append(sim.const_data.snr)
+              count += 1
+            else:
+              if sim[selected_sat].snr is not None:
+                if sim[selected_sat].snr >= snr_threshold and sim[selected_sat].mdp <= mdp_threshold:
+                  mdp_list.append(sim[selected_sat].mdp * 100)
+                  snr_list.append(sim[selected_sat].snr)
+                else:
+                  no_detec[0].append(sim[selected_sat].mdp * 100)
+                  no_detec[1].append(sim[selected_sat].snr)
+                count += 1
 
-# print(val)
-# print(time()-init_time)
-# print(list(zip([1], [2], [3],[1, 1, 1, 1, 1, 1, 1])))
-# pool.starmap(AllSimData, zip(repeat(self.sim_prefix), range(self.n_source), repeat(cat_data), repeat(self.mode), repeat(self.n_sim), repeat(self.sat_info), repeat(self.pol_data), repeat(self.sim_duration), repeat(self.options))
+      distrib, ax1 = plt.subplots(1, 1, figsize=(8, 6))
+      distrib.suptitle("SNR as a function of fluence of detected GRB")
+      if print_rejected:
+        ax1.scatter(no_detec[0], no_detec[1], label="Markers for rejected GRB")
+      ax1.scatter(mdp_list, snr_list, s=3,
+                  label=f'Detected GRB (Both with SNR and MDP) \nRatio of detectable GRB : {len(snr_list) / count}')
+      ax1.set(xlabel="Fluence (erg.cm-2)", ylabel="SNR (dimensionless)", yscale='linear', xscale='linear')
+              # xlim=(10 ** (int(np.log10(np.min(fluence_list)))), 10 ** (int(np.log10(np.max(fluence_list))) + 1)))
+      ax1.legend()
+      plt.show()
