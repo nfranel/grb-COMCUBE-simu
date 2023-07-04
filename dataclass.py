553474770e7c41a70afe313d979a29534af4ce16
nfranel <nathan.franel@ijclab.in2p3.fr>
2023-07-04 11:43:01 +0200

    Resolving a bug in dataclass

--- dataclass.py
+++ dataclass.py
@@ -431,12 +431,11 @@ class FormatedData:
           self.CE = self.CE[inwindow(self.CE_sum, ergcut)]
           self.CE_sum = self.CE_sum[inwindow(self.CE_sum, ergcut)]
         # Warning : the first value of self.CE is the second hit in the detector
-        try:
-          self.polar_from_energy = np.rad2deg(np.arccos(1 - m_elec * c_light ** 2 / charge_elem / 1000 * (1 / self.CE[:, 0] - 1 / (self.CE_sum))))
-        except IndexError:
-          print("files : ", data_list)
-          print("CE :", self.CE)
-          print("CE_sum :", self.CE_sum)
+        print("files : ", data_list)
+        print("CE :", self.CE)
+        print("CE_sum :", self.CE_sum)
+        self.polar_from_energy = np.rad2deg(np.arccos(1 - m_elec * c_light ** 2 / charge_elem / 1000 * (1 / self.CE[:, 0] - 1 / (self.CE_sum))))
+        print("Calculation done \n")
 
       self.compton = np.sum(inwindow(self.CE_sum, ergcut))
       self.cr = self.compton / sim_duration
