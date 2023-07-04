42d106ed20f7d0f2b200fa2dffb1271a45e1cfc1
nfranel <nathan.franel@ijclab.in2p3.fr>
2023-07-04 11:00:54 +0200

    Resolving a bug in dataclass

--- dataclass.py
+++ dataclass.py
@@ -435,7 +435,7 @@ class FormatedData:
           self.polar_from_energy = np.rad2deg(np.arccos(1 - m_elec * c_light ** 2 / charge_elem / 1000 * (1 / self.CE[:, 0] - 1 / (self.CE_sum))))
         except IndexError:
           print("files : ", data_list)
-          print("CE :", self.CE[:, 0])
+          print("CE :", self.CE)
           print("CE_sum :", self.CE_sum)
 
       self.compton = np.sum(inwindow(self.CE_sum, ergcut))
