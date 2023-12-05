47ede49a57d29e928037b75ade22c561cf3feb72
nfranel <nathan.franel@ijclab.in2p3.fr>
2023-12-05 18:41:09 +0100

    Minor changing for testing

--- launch_bkg_sim.py
+++ launch_bkg_sim.py
@@ -36,7 +36,6 @@ def read_par(parfile):
     elif line.startswith("@latitudes"):
       latitudes = list(map(int, line.split(" ")[1:]))
       latitudes = np.linspace(latitudes[0], latitudes[1], latitudes[2])
-  print(geom, revanf, mimrecf, source_base, spectra, simtime, latitudes, altitudes)
   return geom, revanf, mimrecf, source_base, spectra, simtime, latitudes, altitudes
 
 
