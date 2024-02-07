dcd0652fcbaa2ead88bf297b9ca9609f12b03323
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-02-07 11:30:36 +0100

    Updating display methods/functions

--- MLogData.py
+++ MLogData.py
@@ -50,8 +50,9 @@ class LogData:
     self.keys_description = lines[2]
     self.keys = lines[3]
     for line in lines[6:-1]:
-      print(data)
       data = line.split(" | ")
+      print(data)
+
       self.name.append(data[0])
       self.sim_num.append(int(data[1]))
       self.sat_num.append(int(data[2]))
