83f2cc9d17a461ccecd914a340f1a2abaee3dd6b
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-03-04 11:57:40 +0100

    Testing the simulations with light curves

--- launch_sim_time.py
+++ launch_sim_time.py
@@ -139,8 +139,6 @@ def maketmpsf(command, args, pid):
   with open(fname, "w") as f:
     run, source = "", ""  # So that the test with startswith may be done even if run and source are not initialized yet
     for line in lines:
-      if "\n" in line:
-        print("ICIIIII : ", line)
       if line.startswith("Geometry"):
         f.write(f"Geometry {args.geometry}")
       elif line.startswith("PhysicsListEM "):
