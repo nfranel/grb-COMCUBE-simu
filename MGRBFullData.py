eb688b24692fa770f48ac0315b4c702fd8573608
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-03-12 09:56:34 +0100

    Correcting new constellation making

--- MGRBFullData.py
+++ MGRBFullData.py
@@ -341,6 +341,8 @@ class GRBFullData:
     thresh_1024 = 3.6
     thresh_2048 = 3.5
     thresh_4096 = 3.4
-    # faire les tests
-    self.const_beneficial_trigger = True
+    if (self.hits_snrs[0] >= thresh_32 or self.hits_snrs[1] >= thresh_64 or self.hits_snrs[2] >= thresh_128 or self.hits_snrs[3] >= thresh_256 or self.hits_snrs[4] >= thresh_512 or self.hits_snrs[5] >= thresh_1024 or self.hits_snrs[6] >= thresh_2048 or self.hits_snrs[7] >= thresh_4096):
+      self.const_beneficial_trigger = True
+    else:
+      self.const_beneficial_trigger = False
 
