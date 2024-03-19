0e63df75020b5dc4472a371865b3bd366bdad4c9
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-03-19 12:03:03 +0100

    Correcting new constellation making

--- MAllSatData.py
+++ MAllSatData.py
@@ -164,7 +164,7 @@ class AllSatData(list):
           if item in ["bins"]:
             print(self)
             print(selected_sats)
-            print(self[selected_sats])
+            print(self[selected_sats[0]])
             setattr(self.const_data[ite_const], item, getattr(self[selected_sats[0]], item))
           #############################################################################################################
           # Set to true
