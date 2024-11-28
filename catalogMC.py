614529c7e5db0aff23625a8542d10e5fa0e91e07
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-11-28 21:57:33 +0100

    Changing mdp plots

--- catalogMC.py
+++ catalogMC.py
@@ -283,7 +283,7 @@ class MCCatalog:
         else:
           return "0 - -2"
 
-      fold_name = "longredlum480v2"
+      fold_name = "longredlum480"
       savefile = f"Sampled/{fold_name}/longfit_red_lum.csv"
       comm = "Long-Redshift-Luminosity"
       if not (f"{fold_name}" in os.listdir("Sampled/")):
