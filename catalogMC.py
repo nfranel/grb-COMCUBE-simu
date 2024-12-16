0573ba5f92bc555120df0dc634921761e46144e6
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-12-16 11:58:41 +0100

    Working on sampled catalog

--- catalogMC.py
+++ catalogMC.py
@@ -283,7 +283,7 @@ class MCCatalog:
         else:
           return "0 - 2"
 
-      fold_name = "longredlumv3-2220"
+      fold_name = "longredlumv4-2220"
       savefile = f"Sampled/{fold_name}/longfit_red_lum.csv"
       comm = "Long-Redshift-Luminosity"
       if not (f"{fold_name}" in os.listdir("Sampled/")):
