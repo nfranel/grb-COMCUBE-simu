afba5ef9f7b149a8b6c06659b53aeeb33a8d6860
nfranel <nathan.franel@ijclab.in2p3.fr>
2024-12-17 10:13:42 +0100

    Working on sampled catalog

--- catalogMC.py
+++ catalogMC.py
@@ -170,13 +170,13 @@ class MCCatalog:
 
     # Redshift
     self.l_rate_min = 0.2
-    self.l_rate_max = 1.5
+    self.l_rate_max = 1
     self.l_ind1_z_min = 1.5
     self.l_ind1_z_max = 3.4
     self.l_ind2_z_min = -2.4
     self.l_ind2_z_max = -0.01
     self.l_zb_min = 2
-    self.l_zb_max = 5
+    self.l_zb_max = 4
 
     self.s_rate_min = 0.4
     self.s_rate_max = 1.1
@@ -188,11 +188,11 @@ class MCCatalog:
     self.s_zb_max = 3.3
     # Luminosity
     self.l_ind1_min = -1.1
-    self.l_ind1_max = -0.4
+    self.l_ind1_max = -0.6
     self.l_ind2_min = -5
     self.l_ind2_max = -2
     self.l_lb_min = 5e50
-    self.l_lb_max = 6e52
+    self.l_lb_max = 5e52
 
     self.s_ind1_min = -1
     self.s_ind1_max = -0.39
@@ -283,7 +283,7 @@ class MCCatalog:
         else:
           return "0 - 2"
 
-      fold_name = "longredlumv4-2220"
+      fold_name = "longredlumv5-2220"
       savefile = f"Sampled/{fold_name}/longfit_red_lum.csv"
       comm = "Long-Redshift-Luminosity"
       if not (f"{fold_name}" in os.listdir("Sampled/")):
